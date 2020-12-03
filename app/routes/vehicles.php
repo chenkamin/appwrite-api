1487f7b271a9fcd7abd2b1f8e85caba35574219a
chenkamin <chenkamin@gmail.com>
2020-12-04 00:28:17 +0200

    indent problem in github - test num 2

--- app/routes/vehicles.php
+++ app/routes/vehicles.php
@@ -12,65 +12,65 @@ use Utopia\Validator\Range;
 
 
 App::get(BASE_URL .'/vehicles')
-    ->desc('Get all vehicles')
-    ->action(function ($request ,$response) {
-				$id = $request->getParam("id");	
-				$vehicles =	getVehicleById($id);
-				$response
-					->setStatusCode(Response::STATUS_CODE_OK)
-					->json([
-						"data" => ["vehicles" =>$vehicles]]);       
-    }, ['request', 'response']);
+  ->desc('Get all vehicles')
+  ->action(function ($request ,$response) {
+    $id = $request->getParam("id");	
+    $vehicles =	getVehicleById($id);
+    $response
+      ->setStatusCode(Response::STATUS_CODE_OK)
+      ->json([
+      "data" => ["vehicles" =>$vehicles]]);       
+  }, ['request', 'response']);
 
   
 
 App::post(BASE_URL . '/vehicles')
-    ->desc('Create a new vehicle')
-    ->action(function ($request,$response) {
-        //mock insert and validation
-        $vehicle = $request->getPayload('vehicle');
-        $model = $request->getPayload('model');
-        if(!isset($vehicle) || !isset($model)){
-					$response
-            ->setStatusCode(Response::STATUS_CODE_BAD_REQUEST)
-            ->json([	
-							"status" => "fail",
-							"msg" => "vehicle and model are required"
-						]);
-        }
-        $id = "nkjdwq87wqdhwqd6";
-        $newVehicles = ["vehicle"=>$vehicle , "model" =>$model , "id" =>$id];
-        $response
-        	->setStatusCode(Response::STATUS_CODE_CREATED)
-        	->json([
-						"status" => "success",
-						"data" => ["vehicle" =>$newCar]
+  ->desc('Create a new vehicle')
+  ->action(function ($request,$response) {
+    //mock insert and validation
+    $vehicle = $request->getPayload('vehicle');
+    $model = $request->getPayload('model');
+    if(!isset($vehicle) || !isset($model)){
+      $response
+       ->setStatusCode(Response::STATUS_CODE_BAD_REQUEST)
+       ->json([	
+         "status" => "fail",
+         "msg" => "vehicle and model are required"
           ]);
-    }, ['request','response']);
+    }
+    $id = "nkjdwq87wqdhwqd6";
+    $newVehicles = ["vehicle"=>$vehicle , "model" =>$model , "id" =>$id];
+    $response
+      ->setStatusCode(Response::STATUS_CODE_CREATED)
+      ->json([
+        "status" => "success",
+        "data" => ["vehicle" =>$newCar]
+      ]);
+  }, ['request','response']);
 
 App::put(BASE_URL . '/vehicles')
-    ->desc('Update a new vehicle')
-    ->action(function ($request, $response) {
-			$id = $request->getQuery("id");	
-			$vehicles =	getVehicleById($id);
-			$updatedVehicle = $vehicles;
-			$response
-			->setStatusCode(Response::STATUS_CODE_OK)
+  ->desc('Update a new vehicle')
+  ->action(function ($request, $response) {
+    $id = $request->getQuery("id");	
+    $vehicles =	getVehicleById($id);
+    $updatedVehicle = $vehicles;
+    $response
+      ->setStatusCode(Response::STATUS_CODE_OK)
 			->json([
 				"status" => "success",
 				"data" => ["vehicles" =>$updatedVehicle]]);       
 		}, ['request', 'response']);
 
 App::delete(BASE_URL . '/vehicles')
-    ->desc('Delete a vehicle')
-    ->action(function ($request, $response) {
-			$response  	
-				->setStatusCode(Response::STATUS_CODE_NOCONTENT)
-				->json([
-					"status" => "success",
-					"data" => null
-				]);
-		}, ['request', 'response']);
+  ->desc('Delete a vehicle')
+  ->action(function ($request, $response) {
+    $response  	
+      ->setStatusCode(Response::STATUS_CODE_NOCONTENT)
+      ->json([
+        "status" => "success",
+        "data" => null
+      ]);
+  }, ['request', 'response']);
 		
 
 		function getVehicleById($id){
