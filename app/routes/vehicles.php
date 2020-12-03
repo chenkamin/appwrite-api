142d874b72cda344a32ade664d6f3c06e2b36543
chenkamin <chenkamin@gmail.com>
2020-12-04 00:32:07 +0200

    indent problem in github - done

--- app/routes/vehicles.php
+++ app/routes/vehicles.php
@@ -12,58 +12,58 @@ use Utopia\Validator\Range;
 
 
 App::get(BASE_URL .'/vehicles')
-  ->desc('Get all vehicles')
-  ->action(function ($request ,$response) {
-    $id = $request->getParam("id");	
-    $vehicles =	getVehicleById($id);
-    $response
-      ->setStatusCode(Response::STATUS_CODE_OK)
-      ->json([
-      "data" => ["vehicles" =>$vehicles]]);       
+    ->desc('Get all vehicles')
+    ->action(function ($request ,$response) {
+        $id = $request->getParam("id");	
+        $vehicles =	getVehicleById($id);
+        $response
+          ->setStatusCode(Response::STATUS_CODE_OK)
+          ->json([
+            "data" => ["vehicles" =>$vehicles]]);       
   }, ['request', 'response']);
 
   
 
 App::post(BASE_URL . '/vehicles')
-  ->desc('Create a new vehicle')
-  ->action(function ($request,$response) {
+    ->desc('Create a new vehicle')
+    ->action(function ($request,$response) {
     //mock insert and validation
-    $vehicle = $request->getPayload('vehicle');
-    $model = $request->getPayload('model');
-    if(!isset($vehicle) || !isset($model)){
-      $response
-       ->setStatusCode(Response::STATUS_CODE_BAD_REQUEST)
-       ->json([	
-         "status" => "fail",
-         "msg" => "vehicle and model are required"
-          ]);
-    }
-    $id = "nkjdwq87wqdhwqd6";
-    $newVehicles = ["vehicle"=>$vehicle , "model" =>$model , "id" =>$id];
-    $response
-      ->setStatusCode(Response::STATUS_CODE_CREATED)
-      ->json([
-        "status" => "success",
-        "data" => ["vehicle" =>$newCar]
-      ]);
+        $vehicle = $request->getPayload('vehicle');
+        $model = $request->getPayload('model');
+        if(!isset($vehicle) || !isset($model)){
+            $response
+              ->setStatusCode(Response::STATUS_CODE_BAD_REQUEST)
+              ->json([	
+                "status" => "fail",
+                "msg" => "vehicle and model are required"
+            ]);
+        }
+        $id = "nkjdwq87wqdhwqd6";
+        $newVehicles = ["vehicle"=>$vehicle , "model" =>$model , "id" =>$id];
+        $response
+          ->setStatusCode(Response::STATUS_CODE_CREATED)
+          ->json([
+            "status" => "success",
+            "data" => ["vehicle" =>$newCar]
+         ]);
   }, ['request','response']);
 
 App::put(BASE_URL . '/vehicles')
-  ->desc('Update a new vehicle')
-  ->action(function ($request, $response) {
-    $id = $request->getQuery("id");	
-    $vehicles =	getVehicleById($id);
-    $updatedVehicle = $vehicles;
-    $response
-      ->setStatusCode(Response::STATUS_CODE_OK)
-			->json([
-				"status" => "success",
-				"data" => ["vehicles" =>$updatedVehicle]]);       
+    ->desc('Update a new vehicle')
+    ->action(function ($request, $response) {
+        $id = $request->getQuery("id");	
+        $vehicles =	getVehicleById($id);
+        $updatedVehicle = $vehicles;
+        $response
+          ->setStatusCode(Response::STATUS_CODE_OK)
+          ->json([
+            "status" => "success",
+            "data" => ["vehicles" =>$updatedVehicle]]);       
 		}, ['request', 'response']);
 
 App::delete(BASE_URL . '/vehicles')
-  ->desc('Delete a vehicle')
-  ->action(function ($request, $response) {
+    ->desc('Delete a vehicle')
+    ->action(function ($request, $response) {
     $response  	
       ->setStatusCode(Response::STATUS_CODE_NOCONTENT)
       ->json([
@@ -73,14 +73,14 @@ App::delete(BASE_URL . '/vehicles')
   }, ['request', 'response']);
 		
 
-		function getVehicleById($id){
-			$vehicles = json_decode(file_get_contents(MOCK_DATA . '/vehicles.json'), true);
-			if($id){
-				foreach ($vehicles as $key => $v) {
-					if($v['id'] == $id){
-					return	$vehicles = $v;
-					}
-				}
-			}
-			return $vehicles;
-		}
+function getVehicleById($id){
+  $vehicles = json_decode(file_get_contents(MOCK_DATA . '/vehicles.json'), true);
+  if($id){
+    foreach ($vehicles as $key => $v) {
+      if($v['id'] == $id){
+      return $vehicles = $v;
+      }
+    }
+  }
+  return $vehicles;
+}
